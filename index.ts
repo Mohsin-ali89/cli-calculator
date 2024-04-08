c7116dc924cd71bf21aa833611f1727aef778c7f
Mohsin-ali89 <165731342+Mohsin-ali89@users.noreply.github.com>
2024-04-09 00:21:26 +0500

    Hello! everyone I,m a student of governer sindh IT program now I,m learning typescript and this is my first project

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+  const answer = await inquirer.prompt([
+    { message: "enter your first number", type: "number", name: "firstnumber" },
+    {
+      message: "entre your second number",
+      type: "number",
+      name: "secondnumber",
+    },
+    {
+      message: "select one of the operator to perform operation",
+      type: "list",
+      name: "operator",
+      choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+  ]);
+
+  //conditional statement
+
+  if (answer.operator === "addition") {
+    console.log(
+      `The answer of sum is : ${answer.firstnumber + answer.secondnumber}`
+    )
+  } else if (answer.operator === "subtraction") {
+    console.log(
+      `The answer of subtraction is : ${
+        answer.firstnumber - answer.secondnumber
+      }`
+    )
+  }
+ else if (answer.operator === "multiplication") {
+  console.log(
+    `The answer of multiplication is : ${
+      answer.firstnumber * answer.secondnumber
+    }`
+  )
+} else if (answer.operator === "division") {
+  console.log(
+    `The answer of division is : ${(
+
+      answer.firstnumber / answer.secondnumber
+    ).toFixed(4)}`
+  )
+}
